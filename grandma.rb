e1e9c101e3c9a1203e9d05cec9d56adaf3f51a8b
Amanda Ackerman <ackerm44@gmail.com>
2017-10-20 15:40:25 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma(phrase)
     "I LOVE YOU TOO PUMPKIN!"
   elsif phrase.upcase
     "NO, NOT SINCE 1938!"
-  else
+  elsif phrase.downcase
     "HUH?! SPEAK UP, SONNY!"
   end
 end
